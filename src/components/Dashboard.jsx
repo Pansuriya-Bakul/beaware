2f4567a5744114c9ae78146fb26f5c4ae2d744d5
Paurav Thakkar <thakka38@uwindsor.ca>
2024-03-14 20:50:07 -0400

    mobile view

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,23 +1,29 @@
 import React, { useEffect, useState } from 'react';
-import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase/config';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import InputComponent from './Input';
-import logo from '../style/logo.png'
-import login_vec from '../style/login-vec.png'
+import logo from '../style/logo.png';
 import { Row, Col, Nav, Button} from 'react-bootstrap';
 import QRCode from 'qrcode.react';
-import { SketchPicker } from 'react-color'
 
 const Dashboard = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
   const [showStream, setShowStream] = useState(false);
   const [streamName, setStreamName] = useState('');
   const [streamColor, setStreamColor] = useState('');
+  const [isMobile, setIsMobile] = useState(false);
+
+  const componentDidMount = () => {
+    setIsMobile(window.innerWidth < 1200 ? true : false);
+    console.log(window.innerWidth);
+    window.addEventListener('resize', () => {
+      setIsMobile(window.innerWidth < 1200 ? true : false);
+    }, false);
+  };
+
   useEffect(() => {
     // Fetch stream name and hex color from the database or API
     const fetchData = async () => {
@@ -43,6 +49,7 @@ const Dashboard = () => {
     };
     getUserDetails();
     fetchData();
+    componentDidMount();
   }, []);
   const getUserDetails = async () => {
     if (auth.currentUser) {
@@ -52,7 +59,7 @@ const Dashboard = () => {
       setPhone(auth.currentUser.phoneNumber || "1234567890");
     
     } else {
-      navigate('/login');
+      // navigate('/login');
     }
   }
 
@@ -60,7 +67,9 @@ const Dashboard = () => {
     auth.signOut();
     navigate('/login');
   };
-  return (
+  console.log(isMobile);
+  return !isMobile ? (
+    // Desktop View
     <Row className='landing'>
       <Col  className='dashboard-container'>
         <div className="dashboard-navbar">
@@ -142,6 +151,94 @@ const Dashboard = () => {
               <div className='dashboard-buttons'>
               <Button variant="outline-primary" size="lg" type="reset" className={'dashboard-button'}>Cancel</Button>
               <Button variant="primary" type="submit" size="lg" className={'dashboard-submit'}>UPDATE</Button>
+              </div>
+            </form>
+        </div>
+      </Col>}
+    </Row>
+  ) : (
+    // Mobile View
+    <Row className='landing'>
+      <Col  className='dashboard-container'>
+        <div className="dashboard-navbar">
+          <img src={logo} alt="logo" />
+        </div>
+        <Nav variant="tabs" defaultActiveKey="update_profile" className="flex-row">
+          <Nav.Link eventKey="update_profile" onClick={() => setShowStream(false)}>Update Profile</Nav.Link>
+          <Nav.Link eventKey="stream_settings" onClick={() => setShowStream(true)}>Stream Settings</Nav.Link>
+          <div className='log-out-container'>
+            <Button size='sm' variant='outline-secondary' onClick={logout}>LOG OUT</Button>
+          </div>
+        </Nav>
+      </Col>
+      {showStream ? <Col className="dashboard-container-right">
+        <div className="dashboard-form-wrapper">
+        <div className='dashboard-header'>STREAM SETTINGS</div>
+          <InputComponent
+            type={"text"}
+            className={'dashboard-stream-name'}
+            input_id={"stream-name"}
+            placeholder={"Stream Name"}
+            label={"Stream Name"}
+            // onChange={(e) => setName(e.target.value)}
+            // disabled={loading}
+            required={true} />
+            <div className='dashboard-color-picker'>
+
+            Stream Color: {streamColor}
+            <input
+              type={"color"}
+              id={"stream-color"}
+              placeholder={"Enter Color Hex"}
+              onChange={(e) => setStreamColor(e.target.value)}
+              required={true} />
+            </div>
+          <div className='qrcode-container'>
+            <QRCode value={streamName} />
+          </div>
+        </div>
+      </Col>
+      :
+      <Col className="dashboard-container-right">
+      <div className='dashboard-header'>HELLO, <p className='name'>{name.toUpperCase()}</p></div>
+        <div className="dashboard-form-wrapper">
+            <form >
+              <InputComponent
+                type={"email"}
+                input_id={"email"}
+                placeholder={"Enter Your Email"}
+                value={email}
+                label={"Email"}
+                v
+                onChange={(e) => setEmail(e.target.value)}
+                required={false} />
+                <InputComponent
+                type={"name"}
+                input_id={"name"}
+                placeholder={"Enter Your Name"}
+                label={"name"}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required={false} />
+                <InputComponent
+                type={"phone"}
+                input_id={"phone"}
+                placeholder={"Enter Your Phone number"}
+                label={"Phone Number"}
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+                required={false} />
+
+              <InputComponent
+                type={"password"}
+                input_id={"password"}
+                label={"Password"}
+                disabled={true}
+                value={"*************"}
+                required={true} />
+              <div className='dashboard-buttons'>
+                <Button variant="outline-primary" size="sm" type="reset" className={'dashboard-button'}>Cancel</Button>
+                <Button variant="primary" type="submit" size="sm" className={'dashboard-submit'}>UPDATE</Button>
               </div>
             </form>
         </div>
